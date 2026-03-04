a013a9ab4e706f5d07ac7a3fcfbfa09d73a40a4a
ewxxd <evdh99@gmail.com>
2026-03-04 12:15:38 +0800

    Add files via upload

---
+++ Task1.1Lights_ON/Task1.1Lights_ON.ino
@@ -0,0 +1,43 @@
+#define LED_PORCH 8
+#define LED_HALLWAY 7
+#define BUTTON_PIN 5
+
+const unsigned long timerThirtySec = 30000;
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(LED_PORCH, OUTPUT);
+  pinMode(LED_HALLWAY, OUTPUT);
+  pinMode(BUTTON_PIN, INPUT);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+
+  if (buttonPressed()){
+    turnOnAllLights();
+    delay(timerThirtySec);
+    turnOffOneLight(LED_PORCH);
+    turnOnOneLight(LED_HALLWAY);
+    delay(timerThirtySec);
+    turnOffOneLight(LED_HALLWAY);
+  }
+}
+
+bool buttonPressed(){
+  return digitalRead(BUTTON_PIN) == HIGH;
+}
+
+void turnOnAllLights(){
+  digitalWrite(LED_PORCH, HIGH);
+  digitalWrite(LED_HALLWAY, HIGH);
+}
+
+void turnOffOneLight(int ledName){
+  digitalWrite(ledName, LOW);
+}
+
+void turnOnOneLight(int ledName){
+  digitalWrite(ledName, HIGH);
+}
